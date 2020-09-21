2fb2ea64cd5a2f3555706cbccca6600a6e59de93
John Behan <behanj@gmail.com>
2020-09-21 20:57:01 +0100

    setup basic fetch

---
+++ src/get_dail_votes.ts
@@ -0,0 +1,13 @@
+/**
+ * Get all votes
+ */
+
+export default async (startDate: string, endDate: string) => { // todo - data type
+  // todo try catch
+  const url =
+    `https://api.oireachtas.ie/v1/debates?chamber_type=house&chamber=dail&date_start=${startDate}&date_end=${endDate}`;
+  const res = await fetch(url);
+
+  const body = await res.json();
+  return body;
+};
